c36ef0ab5303a446979efcc3a7954c7a917ad48f
tylerzellers <tyler.zellers@analygence.com>
2025-03-11 16:43:19 -0400

    about section

--- src/components/SidebarPhoto.js
+++ src/components/SidebarPhoto.js
@@ -4,10 +4,7 @@ export default function SidebarPhoto() {
             <ul className="text-left space-y-2"> 
                 <li>2025
                     <div className="grid grid-cols-2 gap-2">
-                        <img src="https://transgressiverecords.com/wp-content/uploads/2023/09/Screenshot-2025-01-30-at-10.44.52.png" alt="Photo 1" className="w-full h-auto rounded" />
-                        <img src="https://via.placeholder.com/100" alt="Photo 2" className="w-full h-auto rounded" />
-                        <img src="https://via.placeholder.com/100" alt="Photo 3" className="w-full h-auto rounded" />
-                        <img src="https://via.placeholder.com/100" alt="Photo 4" className="w-full h-auto rounded" />
+                        <img src="https://transgressiverecords.com/wp-content/uploads/2023/09/Screenshot-2025-01-30-at-10.44.52.png" alt="press release"/>
                     </div>
                 </li>
                 <li>2024</li>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,3 +1,29 @@
+import { Mail } from "lucide-react";
+import "./About.css";
+
 export default function About(){
-    return <h1>preserving the internet, forever</h1>;
+    return (
+        <>
+        <div align="left">
+            <div>This website was created as a personal project to better my web development skills as well as contribute towards my interest in data preservation for one of my favorite bands. I am not profiting off of any of the content shared on this website and will take content down if requested.</div>
+            <h1>Contact</h1>
+            <div>Contributions are very much welcome! If you have any media that is not featured on this site, please feel free to email us at: xxxxx@gmail.com</div>
+            <br/>
+            <div>Issues regarding the website can also be emailed, but I am trying to model an open source project, so issues filed on github are vastly preferred!</div>
+        </div>
+           <h1>preserving the internet, forever</h1>
+
+        <div className="about-container">
+            <a href="https://github.com/tylerzellers" target="_blank" rel="noopener noreferrer" className="about-link">
+                <img src="https://github.com/tylerzellers.png" alt="GitHub Profile" className="about-icon" />
+                <span>GitHub</span>
+            </a>
+
+            <a href="mailto:your.email@example.com" className="about-link">
+                <Mail className="about-icon" size={48} />
+                <span>Email</span>
+            </a>
+        </div>
+        </>
+    );
 }
